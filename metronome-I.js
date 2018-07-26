3c14168aa7f46e9a11c32bbb8f52782e3c08cba6
Pfuufy <gabrielbissey@gmail.com>
2018-07-25 23:19:50 -0600

    Update metronome-I.js

--- metronome-I.js
+++ metronome-I.js
@@ -1,23 +1,40 @@
+//Only current bug as far as I can tell is that you can have multiple
+//beats playing at one time. Also, I want the metronome to start when
+//"enter" is pressed.
 
-var bpmId;
+var BPMID;
+
 
 function play() {                             //Simply plays a beep when invoked
   var audio = new Audio("beep.flac");
   audio.play();
 }
 
+
 var calcdBpm = function() {                          //Calculates the amount of
   var bpm = document.getElementById("input").value;  //milliseconds based on the BPM
-  var milliSecs = ((60 / bpm) * 1000);
-  return milliSecs;
-}
+  var milliSecs;
+  if (bpm.length == 0) {
+    return false;
+  } else {
+    milliSecs = ((60 / bpm) * 1000);
+    return milliSecs;
+  }
+};
+
 
 function start() {
-  bpmId = setInterval(play, calcdBpm());
-  var bpm = document.getElementById("input").value;
-  document.getElementById("bigText").innerHTML = bpm;  //This little part is for fun
+  var finalBpm = calcdBpm();
+  if (finalBpm === false){
+    document.getElementById("bigText").innerHTML = "Please define a tempo.";
+  } else {
+    BPMID = setInterval(play, finalBpm);
+    var bpm = document.getElementById("input").value;
+    document.getElementById("bigText").innerHTML = bpm;  //This little part is for fun
+  }
 }
 
+
 function stop() {
-  clearInterval(bpmId);
+  clearInterval(BPMID);
 }
